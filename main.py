d1dd35b153a41cd7586788cce7ddcac4ba57b4b2
raghadr <raghadrav@gmail.com>
2022-08-03 00:51:49 +0300

    Conditionals lab

---
+++ main.py
@@ -0,0 +1,19 @@
+Movie = "Hush"
+Rate = 3
+Score = 72.65
+
+if Rate >= 4 and Score > 80 :
+    print("Highly recommended")
+
+elif Rate >=3 and Score > 70:
+    print("I recommended it, It is good")
+
+elif Rate <= 2 and Score>60 :
+    print("You should check it out!")
+
+else:
+    Rate <= 2 and Score<50
+    print("Don't watch it, It is a waste of time")
+
+
+
